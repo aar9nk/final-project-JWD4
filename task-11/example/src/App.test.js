6319ae88932bfe7acb41e2b2cde0f63b84ae96e4
aar9nk <aaronbyrom@protonmail.com>
2020-12-23 18:46:21 +1100

    add tests for taskmanager

--- task-11/example/src/App.test.js
+++ task-11/example/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Task App/i);
   expect(linkElement).toBeInTheDocument();
 });
